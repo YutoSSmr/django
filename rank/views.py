ed76cb53d648aaab8cb70e7ea995336ce7308498
sasa <yuto.job0703@gmail.com>
2022-12-19 22:51:16 +0900

    20221219_pagemovetest

--- rank/views.py
+++ rank/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+ 
+def index_template(request):
+    return render(request, 'index.html')
